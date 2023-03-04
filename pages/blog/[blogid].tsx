3bce10eccf59d2f0488f7bf6739bf2081be1499a
tianzhu <wangtianzhu01@huohua.cn>
2023-03-04 23:51:57 +0800

    add blog article seo

--- pages/blog/[blogid].tsx
+++ pages/blog/[blogid].tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Head from 'next/head'
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import * as prism from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import * as hljs from 'react-syntax-highlighter/dist/cjs/styles/hljs';
@@ -15,11 +16,13 @@ export default function Blog () {
     const router = useRouter();
     const { blogid } = router.query
     const [blog, setBlog] = useState('') 
+    const [rawData, setRawData] = useState('') 
 
     const initBlog = async (blogid: any) => {
         const blogs =  await origin_blog({ blogid })
 
         if (blogs.data.length) {
+            setRawData(blogs.data[0])
             setBlog(blogs.data[0].text)
         }
     }
@@ -32,31 +35,39 @@ export default function Blog () {
 
     return (
         <div style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <ReactMarkdown 
-                className={["markdown-body", style.reactMarkDown].join(' ')}
-                components={{
-                    code({ node, inline, className, children, ...props }) {
-                      const match = /language-(\w+)/.exec(className || '');
-                      return !inline && match ? (
-                        <SyntaxHighlighter
-                          showLineNumbers={true}
-                          style={ prism.darcula }
-                          language={match[1]}
-                          PreTag='div'
-                          {...props}
-                        >
-                          {String(children).replace(/\n$/, '')}
-                        </SyntaxHighlighter>
-                      ) : (
-                        <code className={className} {...props}>
-                          {children}
-                        </code>
-                      );
-                    }
-                  }}
-                >
-                    { blog }
-            </ReactMarkdown>
+          <Head>
+            <title>{ rawData.title }</title>
+            <meta name="description" content={rawData.description} />
+            <meta keywords={rawData.keywords}></meta>
+            <meta name="google" content="notranslate" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <ReactMarkdown 
+              className={["markdown-body", style.reactMarkDown].join(' ')}
+              components={{
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || '');
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        showLineNumbers={true}
+                        style={ prism.darcula }
+                        language={match[1]}
+                        PreTag='div'
+                        {...props}
+                      >
+                        {String(children).replace(/\n$/, '')}
+                      </SyntaxHighlighter>
+                    ) : (
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    );
+                  }
+                }}
+              >
+                  { blog }
+          </ReactMarkdown>
         </div>
     )
 }
