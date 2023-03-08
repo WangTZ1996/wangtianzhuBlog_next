fc05bb49eabbba79a6647637d2bc3a2c06de02b7
tianzhu <wangtianzhu01@huohua.cn>
2023-03-08 10:38:55 +0800

    change api url

--- utils/request.ts
+++ utils/request.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const request = axios.create({
-    // baseURL: 'https://blog.wangtz.cn:8089',
-    baseURL: 'http://127.0.0.1:8089',
+    baseURL: 'https://blog.wangtz.cn:8089',
+    // baseURL: 'http://127.0.0.1:8089',
     timeout: 0,
     headers: {}
 })
